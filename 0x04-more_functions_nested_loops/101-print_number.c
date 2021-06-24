aa1064a79b0df18246b61dcef6cf0cda5a8ecd78
Hezbon12 <otienohezbon12@gmail.com>
2021-06-24 23:05:50 +0300

    nested-13

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
- * Description: Can only use _putchar to print.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-long m; /* power of 10 */
-int c; /* boolean check */
-long num; /* convert int to long */
+unsigned int digit = n;
 
-num = n;
-/* negatives */
-if (num < 0)
+if (n < 0)
 {
-num *= -1;
 _putchar('-');
+digit = -digit;
 }
 
-/* count up */
-m = 1;
-c = 1;
-while (c)
-{
-if (num / (m * 10) > 0)
-m *= 10;
-else
-c = 0;
-}
+if ((digit / 10) > 0)
+print_number(digit / 10);
 
-/* count down */
-while (num >= 0)
-{
-if (m == 1)
-{
-_putchar(num % 10 + '0');
-num = -1;
-}
-else
-{
-_putchar((num / m % 10) + '0');
-m /= 10;
-}
-}
+_putchar((digit % 10) + '0');
 }
