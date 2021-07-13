edaf48ce4a7e361e37bf278fcd799d0a47803e99
Hezbon12 <otienohezbon12@gmail.com>
2021-07-13 20:23:53 +0300

    Write a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,22 +1,63 @@
-#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "holberton.h"
+
 /**
- * argstostr - concatenate all arguments of your program with newline
- * @ac: argument count
- * @av: double pointer to array of strings passed to main
- * Return: Null if fail, else return pointer to new string
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
  */
 
 char *argstostr(int ac, char **av)
 {
-char *a, *retp;
-int i, j, total;
+	char *s;
+	int len = 0, i, j, k = 0;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	if (ac == 0 || av == NULL) /* validate input */
+		return (NULL);
 
-for (i = 0, total = 0; i < ac; i++)
-{
-for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+	/* find length to malloc */
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1); /* add space for newlines and null terminator */
 
+	/* allocate memory and free if error */
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+
+	/* insert each arg into *str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
+
+	return (s);
+}
