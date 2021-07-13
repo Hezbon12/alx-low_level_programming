bbfd32e12bd920cfe1932eaee6d413f6db336bcd
Hezbon12 <otienohezbon12@gmail.com>
2021-07-13 20:16:43 +0300

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,26 +19,4 @@ return (NULL);
 for (i = 0, total = 0; i < ac; i++)
 {
 for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-			;
-total++;
-}
-total++;
 
-a = malloc(total * sizeof(char));
-if (a == NULL)
-return (NULL);
-
-retp = a;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-*a = av[i][j];
-a++;
-}
-*a = '\n';
-a++;
-}
-
-return (retp);
-}
