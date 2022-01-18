0c6df1bb0a805d846059e9b9740f8a36f48d52d0
Hezbon12 <otienohezbon12@gmail.com>
2022-01-18 23:46:32 +0300

    search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,16 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/* helpers */
-void print_array(int *array, size_t inc, size_t size);
-
-/* main functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
+int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
 #endif
